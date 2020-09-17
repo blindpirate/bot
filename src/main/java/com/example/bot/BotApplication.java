6798e2ed5899df42bc7096305cef05ea93fb937d
Bo Zhang <zhangbodut@gmail.com>
2020-09-17 20:41:26 +0800

    Print event type

--- src/main/java/com/example/bot/BotApplication.java
+++ src/main/java/com/example/bot/BotApplication.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 1. 当有人来提交一个PR/MR的时候，自动打上一个标签：waiting-for-review
+ * 2. 如果有人review过代码，他可以在评论中打一个/lgtm
+ *    此时，若该PR通过了CI，则机器人执行自动的合并
+ */
 @SpringBootApplication
 @RestController
 public class BotApplication {
@@ -14,7 +21,8 @@ public class BotApplication {
     }
 
     @PostMapping("/bot")
-    public String endpoint(@RequestBody String body) {
+    public String endpoint(@RequestBody String body, HttpServletRequest request) {
+        System.out.println(request.getHeader("X-GitHub-Event"));
         System.out.println(body);
         return "OK";
     }
